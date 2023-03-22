4b3dfbcfd054b01f95993d2a50fcc843e65b3bb1
U190709050 <elf.prlk64@gmail.com>
2023-03-22 11:24:24 +0300

    threading project done

---
+++ ThreadinProject/ThreadinProject/ViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  ThreadinProject
+//
+//  Created by Elif Bilge Parlak on 22.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    let urlStrings = ["https://images.unsplash.com/photo-1543310465-f4d3ca5a2a25?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80","https://images.unsplash.com/photo-1524079916716-29888bd1b3b5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80","https://images.unsplash.com/photo-1488215545351-57e673dc3789?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1139&q=80","https://images.unsplash.com/photo-1546872863-e85d5c3e5159?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"]
+    
+    var data = Data()
+    var tracker = 0
+
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        DispatchQueue.global().async {
+            self.data = try! Data(contentsOf: URL(string: self.urlStrings[self.tracker])!)
+            DispatchQueue.main.async {
+                self.imageView.image = UIImage(data: self.data)
+            }
+        }
+        
+         
+        
+        
+        self.navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .compose, target: self, action: #selector(changeImage))
+    }
+    
+    @objc func changeImage(){
+        if tracker == 0{
+            tracker += 1
+        }else{
+            tracker -= 1
+        }
+        DispatchQueue.global().async {
+            self.data = try! Data(contentsOf: URL(string: self.urlStrings[self.tracker])!)
+            DispatchQueue.main.async {
+                self.imageView.image = UIImage(data: self.data)
+            }
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Threding Test"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+}
+
